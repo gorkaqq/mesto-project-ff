9aefb21d8bc7d8750ab32b1d06e26b772ea79975
gorkaqq <gorka1233@gmail.com>
2024-03-18 23:37:00 +0400

    complete task

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const cardsConteiner = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+const addCard = function (cardImage, cardTitle, removeCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
+  cardElement.querySelector('.card__image').setAttribute('src', cardImage);
+  cardElement.querySelector('.card__description').textContent = cardTitle;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  deleteButton.addEventListener('click', removeCard);
+
+  cardsConteiner.append(cardElement);
+};
 // @todo: Функция удаления карточки
-
+const removeCard = function (evt) {
+  evt.target.parentElement.remove();
+};
 // @todo: Вывести карточки на страницу
+for (const card of initialCards) {
+  addCard(card.link, card.name, removeCard);
+}
