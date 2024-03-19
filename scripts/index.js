64007e7cc787b6f5a30926e4a1336caa24ac0c26
gorkaqq <gorka1233@gmail.com>
2024-03-19 21:46:21 +0400

    откорректировано по замечаниям

--- scripts/index.js
+++ scripts/index.js
@@ -4,18 +4,20 @@
 const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const addCard = function (cardImage, cardTitle, removeCard) {
+const createCard = function (cardImage, cardTitle, removeCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImageElement = cardElement.querySelector('.card__image');
 
-  cardElement.querySelector('.card__image').setAttribute('src', cardImage);
+  cardImageElement.setAttribute('src', cardImage);
+  cardImageElement.setAttribute('alt', `Фотография ${cardTitle}`);
   cardElement.querySelector('.card__description').textContent = cardTitle;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
 
   deleteButton.addEventListener('click', removeCard);
 
-  cardsContainer.append(cardElement);
+  return cardElement;
 };
 // @todo: Функция удаления карточки
 const removeCard = function (evt) {
@@ -23,5 +25,5 @@ const removeCard = function (evt) {
 };
 // @todo: Вывести карточки на страницу
 for (const card of initialCards) {
-  addCard(card.link, card.name, removeCard);
+  cardsContainer.append(createCard(card.link, card.name, removeCard));
 }
